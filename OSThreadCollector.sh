ef64bf856de2098d6c60a39a214a377828228052
ziwdon <ziwdon@gmail.com>
2019-10-17 10:08:52 +0800

    Add files via upload

--- OSThreadCollector.sh
+++ OSThreadCollector.sh
@@ -2,11 +2,10 @@
 
 # Change log
 # v1.0  * initial version
+# v1.1  * collect thread dumps from outsystems services
 
-# Configurable variables
-VERSION="1.0"
+VERSION="1.1"
 
-# Prepare for execution
 if [ ! -f /etc/sysconfig/outsystems ]; then
 	echo "OutSystems Platform is not installed on this server. Cancelling."
 	exit
@@ -17,44 +16,167 @@ if [ $(whoami) != "root" ]; then
 	exit
 fi
 
+
 source /etc/sysconfig/outsystems
+
+echo ""
+echo "OutSystems Thread Collector v$VERSION"
+
+THREAD_FOLDER="thread_dumps"
+
+# Application server info
+PROCESS_USER=""
+APPSERVER_NAME=""
+
+WEBLOGIC_NAME="WebLogic"
+JBOSS_NAME="JBoss"
+WILDFLY_NAME="Wildfly"
+
+WL_ADMIN_SERVER_NAME="AdminServer"
+
+# Number of total thread collections - Defaults to 1
+COLLECT_TOTAL=1
+
+# Time interval in seconds between thread collections - Defaults to 1 second
+COLLECT_INTERVAL=1
+
+
+# Check arguments for optional configurations
+while [[ $# -gt 0 ]]
+do
+	option="$1"
+
+	case $option in
+		-n|--number)
+			COLLECT_TOTAL=$2
+			shift
+			;;
+		-i|--interval)
+			COLLECT_INTERVAL=$2
+			shift
+			;;
+		-h|--help)
+			echo "Executing this script will collect a thread dump of your application server process. This works for WildFly, WebLogic and JBoss."
+			echo "Options:"
+			echo "    -n|--number [number_of_collections]: Allows you to perform sequential thread collections. The default number of collections is $COLLECT_TOTAL."
+			echo "    -i|--interval [interval_in_seconds]: Interval between consecutive thread collections, specified in seconds. The default interval is $COLLECT_INTERVAL seconds."
+			echo ""
+			exit
+			;;
+		*)
+			# unknown option
+			;;
+	esac
+	shift # past argument or value
+done
+
+
+
+
+# Create temporary folder
 DIR=$(mktemp -d)
 chmod 777 $DIR
 touch $DIR/errors.log
 chmod 777 $DIR/errors.log
 
-LOGS_FOLDER=""
 
+# Check which application server is the server running
+if [ "$JBOSS_HOME" != "" ]; then
+	APPSERVER_NAME="$JBOSS_NAME"
+	PROCESS_USER="jboss"
+	if [ -d $JBOSS_HOME/server/outsystems/ ]; then
+		PROCESS_PID=$(ps -u $PROCESS_USER 2>>/dev/null | grep java | gawk '{print $1}')
+	else
+		if [ -f /var/run/jboss-as/jboss-as-standalone-outsystems.pid ]; then
+			PROCESS_PID=$(cat /var/run/jboss-as/jboss-as-standalone-outsystems.pid)
+		else
+			PROCESS_PID=$(ps -ef | grep java.*standalone-outsystems.xml | grep -v grep | awk '{print $2}')
+		fi
+	fi
+fi
+
+if [ "$WILDFLY_HOME" != "" ]; then
+       APPSERVER_NAME=$WILDFLY_NAME
+       PROCESS_USER="wildfly"
+       
+       PROCESS_PID=$(ps -ef | grep java.*standalone-outsystems.xml | grep -v grep | awk '{print $2}')
+       JBOSS_HOME=$WILDFLY_HOME
+fi
+
+if [ "$WL_DOMAIN" != "" ]; then
+	APPSERVER_NAME="$WEBLOGIC_NAME"
+	if [ "$PROCESS_USER" == "" ]; then
+		PROCESS_USER=$(stat -c %U $WL_DOMAIN)
+	fi
+	
+	if [ "$WL_MANAGED_SERVER_NAME" == "" ]; then
+		PROCESS_PID=$(ps -u $PROCESS_USER --format "pid cmd" 2>>/dev/null | grep java | grep weblogic.Server | grep -v weblogic.Name=$WL_ADMIN_SERVER_NAME | gawk '{print $1}')
+		WL_MANAGED_SERVER_NAME=$(ps --pid $PROCESS_PID --format cmd | grep java | sed 's/.*weblogic.Name=[ ]*\([^ ]*\).*/\1/g')
+	else
+		PROCESS_PID=$(ps -u $PROCESS_USER --format "pid cmd" 2>>/dev/null | grep java | grep weblogic.Server | grep weblogic.Name=$WL_MANAGED_SERVER_NAME | gawk '{print $1}')
+	fi
+fi
+
+# Get java bin
 JAVA_BIN=$(dirname "$(readlink /proc/$PROCESS_PID/exe)")
+
 if [ -f $JAVA_BIN/../../bin/java ]; then
 	JAVA_BIN="$JAVA_BIN/../../bin/"
 fi
 
-echo ""
-echo "OutSystems Services Thread Collector v$VERSION" >> $DIR/toolinfo
-echo >> $DIR/toolinfo
-echo "OutSystems platform Directory: $OUTSYSTEMS_HOME" >> $DIR/toolinfo
 
-if [ -h /opt -o -h /opt/outsystems -o -h /opt/outsystems/platform -o -h /opt/outsystems/platform/share ]; then
-	echo >> $DIR/toolinfo
-	echo "WARNING: OutSystems is installed on a symlink." >> $DIR/toolinfo
-	echo "  From version 9.1 this may make it impossible to publish modules with web references." >> $DIR/toolinfo
-	echo >> $DIR/toolinfo
+mkdir -p thread_dumps/
+
+# Collect the thread dumps
+if [ "$PROCESS_PID" == "" ]; then
+	echo "Could not find the $APPSERVER_NAME process."
+else
+	COLLECT_COUNT=0
+	
+	while [ $COLLECT_COUNT -lt $COLLECT_TOTAL ]; do
+		let collect_print=COLLECT_COUNT+1
+		
+		TIMESTAMP=$(date +%F_%H%M%S)
+		
+		# Collect application server threads
+		echo "Collecting $APPSERVER_NAME threads $collect_print of $COLLECT_TOTAL..."
+		if [ -f $JAVA_BIN/jrcmd ]; then
+			su $PROCESS_USER - -s /bin/bash -c "$JAVA_BIN/jrcmd $PROCESS_PID print_threads" > $DIR/threads_"$APPSERVER_NAME".log 2>> $DIR/errors.log
+		else
+			su $PROCESS_USER - -s /bin/bash -c "$JAVA_BIN/jstack $PROCESS_PID" > $DIR/threads_"$APPSERVER_NAME".log 2>> $DIR/errors.log
+		fi
+		FILENAME="$APPSERVER_NAME_"$TIMESTAMP".log"
+		cp $DIR/threads_$APPSERVER_NAME.log $THREAD_FOLDER/$FILENAME;
+		
+		# Collect OutSystems services threads
+		for SERVICE_INFO in $(su outsystems -c "$JAVA_HOME/bin/jps -l" | grep outsystems.hubedition | tr ' ' '|')
+		do
+			echo "Collecting $SERVICE_PROCESS_NAME threads $collect_print of $COLLECT_TOTAL..."
+			eval $(echo "$SERVICE_INFO" | gawk -F "|" '{print "SERVICE_PID="$1";SERVICE_PROCESS_NAME="$2}')
+			if [ -f $JAVA_HOME/bin/jrcmd ]; then
+				su outsystems - -s /bin/bash -c "$JAVA_HOME/bin/jrcmd $SERVICE_PID print_threads > $DIR/threads_"$SERVICE_PROCESS_NAME".log 2>> $DIR/errors.log"
+			else
+				su outsystems - -s /bin/bash -c "$JAVA_HOME/bin/jstack $SERVICE_PID > $DIR/threads_"$SERVICE_PROCESS_NAME".log 2>> $DIR/errors.log"
+			fi
+			pmap -d $SERVICE_PID > $DIR/pmap_$SERVICE_PROCESS_NAME
+			
+			FILENAME="$SERVICE_PROCESS_NAME_"$TIMESTAMP".log"
+			cp $DIR/threads_"$SERVICE_PROCESS_NAME".log $THREAD_FOLDER/$FILENAME;
+			
+		done
+				
+		
+		echo "Threads collected successfully. You can find them in $THREAD_FOLDER/$FILENAME"
+		
+		let COLLECT_COUNT=COLLECT_COUNT+1
+		
+		if [ $COLLECT_TOTAL -gt 1 -a $COLLECT_COUNT -lt $COLLECT_TOTAL ]; then
+			echo "Waiting $COLLECT_INTERVAL seconds for next collection."
+			sleep $COLLECT_INTERVAL
+		fi
+	done
+	
 fi
 
-echo "Java Directory: $JAVA_BIN"  >> $DIR/toolinfo
-cat $DIR/toolinfo
-echo
-
-# Collect threads
-echo "Gathering OutSystems Services info ..."
-for SERVICE_INFO in $(su outsystems -c "$JAVA_HOME/bin/jps -l" | grep outsystems.hubedition | tr ' ' '|')
-do
-	eval $(echo "$SERVICE_INFO" | gawk -F "|" '{print "SERVICE_PID="$1";SERVICE_PROCESS_NAME="$2}')
-	if [ -f $JAVA_HOME/bin/jrcmd ]; then
-		su outsystems - -s /bin/bash -c "$JAVA_HOME/bin/jrcmd $SERVICE_PID print_threads > $DIR/threads_"$SERVICE_PROCESS_NAME".log 2>> $DIR/errors.log"
-	else
-		su outsystems - -s /bin/bash -c "$JAVA_HOME/bin/jstack $SERVICE_PID > $DIR/threads_"$SERVICE_PROCESS_NAME".log 2>> $DIR/errors.log"
-	fi
-	pmap -d $SERVICE_PID > $DIR/pmap_$SERVICE_PROCESS_NAME
-done
+# Delete the previously generated temporary folder
+rm -rf $DIR
